ea0685f4e06f887f4419b70e1284f551b67005ca
zeno <Zeno_94@outlook.com>
2017-05-22 23:31:04 +0800

    server端打包

---
+++ MessageServer/MessageServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageServer
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(String[] args)
+        {
+            
+              // 通过windows窗体启动服务
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MessageServer());
+               // Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+        }
+    }
+}
